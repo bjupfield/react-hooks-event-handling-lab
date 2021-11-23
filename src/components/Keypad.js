94ab313dd4c8086143a7504ad1b57a1ef967c6f9
Bennett <bjupfield@gmail.com>
2021-11-23 12:28:29 -0600

    I finally completed the deliverables and the test

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,10 @@
 // Code Keypad Component Here
+function Keypad(){
+    return(
+        <input onChange = {()=>{
+            console.log("Entering password...")
+        }} type = "password">
+        </input>
+    )
+}
+export default Keypad;
